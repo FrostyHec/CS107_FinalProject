0625d3803d3f246a4e340d22f963952915c90728
Frosty <3259024151@qq.com>
2022-11-17 22:40:09 +0800

    完成了最基本的面板设置 Signed-off-by: Frosty <3259024151@qq.com>

---
+++ src/main/java/Entry/Game.java
@@ -0,0 +1,38 @@
+package Entry;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Game extends Application {
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        //窗体基本属性
+        stage.setTitle("DarkChess");
+        stage.setResizable(false);
+
+        //加载fxml
+        FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("GameArea.fxml"));
+        stage.setScene(new Scene(fxmlLoader.load(), 600, 800));
+        stage.show();
+    }
+
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/Entry/GameArea.java
+++ src/main/java/Entry/GameArea.java
@@ -1,22 +1,12 @@
 package Entry;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.fxml.FXML;
 
-import java.io.IOException;
+public class GameArea {
 
-public class GameArea extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameArea.class.getResource("hello-view.fxml"));
-        stage.setTitle("DarkChess");
-        stage.setScene(new Scene(fxmlLoader.load(), 320, 240));
-        stage.show();
-    }
+    @FXML
+    protected void squareClick() {
+        System.out.println("HelloWorld!");
 
-    public static void main(String[] args) {
-        launch();
     }
 }

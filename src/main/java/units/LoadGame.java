4fd30fb3c8adc7ce535d08e85c9669d5cfb6fabf
redhecker <112929191+redhecker@users.noreply.github.com>
2022-11-25 21:35:22 +0800

    Add files via upload

--- src/main/java/units/LoadGame.java
+++ src/main/java/units/LoadGame.java
@@ -1,4 +1,31 @@
 package units;
 
+import GameLogic.*;
+import java.io.*;
+
 public class LoadGame {
+    public static void main(String[] args) {
+        Game e = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("D:\\files\\game.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            e = (Game) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Game class not found");
+            c.printStackTrace();
+            return;
+        }
+        System.out.println(e.getMoves());
+        for(Chess[] c : e.getChess()){
+            for(Chess cc : c){
+                System.out.print(cc.getColor());
+                System.out.println(cc.getRank());
+            }
+        }
+    }
 }
--- src/main/java/units/unitTest.java
+++ src/main/java/units/unitTest.java
@@ -5,6 +5,7 @@ import GameLogic.Game;
 public class unitTest {
     public static void main(String[] args) {
         Game game = new Game();
+        game.init();
         SaveGame.save(game);
     }
 }

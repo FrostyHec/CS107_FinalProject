6d76f6ca34244dc20b21da2f497ac5ed98ea00b4
Frosty <3259024151@qq.com>
2022-11-29 14:10:46 +0800

    一些更新

--- src/main/java/Windows/StartMenu/Main.java
+++ src/main/java/Windows/StartMenu/Main.java
@@ -9,13 +9,14 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     private NormalSettings settings;
+
     @Override
     public void start(Stage stage) throws Exception {
         //窗体基本属性
         stage.setTitle("DarkChess");
         stage.setResizable(false);
         //加载fxml
-        settings =NormalSettings.readSettings(NormalSettings.url);
+        settings = NormalSettings.readSettings(NormalSettings.url);
         Application.setUserAgentStylesheet(getClass().getResource(settings.startMenu.getSkin()).toExternalForm());
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StartMenu.fxml"));
         stage.setScene(new Scene(fxmlLoader.load(), 600, 400));
